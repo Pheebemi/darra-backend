e6374e5b5b0b2030934b54699c822b647d2ed81f
pheebemi <lemuelemmanuel29@gmail.com>
2025-11-13 16:19:42 +0100

    kj;w;ew

--- web/src/lib/api/client.ts
+++ web/src/lib/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "https://darra.vercel.app/api";
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "https://pheebemi.pythonanywhere.com/api";
 
 export const apiClient = axios.create({
   baseURL: apiBaseUrl,
